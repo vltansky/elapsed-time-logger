b952df3b47bd8ec0edbddd83d3dced5b77d23f1a
Vlad <vl.tansky@gmail.com>
2020-04-20 20:11:49 +0300

    refactor

--- tests/class.js
+++ tests/class.js
@@ -1,9 +1,7 @@
 const { TimeLogger } = require("../lib/index");
 
-(() => {
-    const elapsed = new TimeLogger();
-    console.log('smth');
-    setTimeout(()=>{
-        elapsed.end('finished:');
-    }, 60000)
-})();
+const elapsed = new TimeLogger();
+console.log('smth');
+setTimeout(()=>{
+    elapsed.end('finished:');
+}, 60000)
--- tests/simple.js
+++ tests/simple.js
@@ -1,9 +1,7 @@
 const { consoleTimeLogger } = require("../lib/index");
 
-(() => {
-    consoleTimeLogger.start('test');
-    console.log('smth');
-    setTimeout(()=>{
-        consoleTimeLogger.end();
-    }, 60000)
-})();
+consoleTimeLogger.start('test');
+console.log('smth');
+setTimeout(()=>{
+    consoleTimeLogger.end();
+}, 60000)
