149f9c40eab41105b071b71f6fefec71f2743e7f
Vlad <vl.tansky@gmail.com>
2020-04-20 20:36:33 +0300

    format

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,68 @@
 const convertHrTime = (hrtime: any) => {
-    let result = '';
-    const sourceMS: number = ((hrtime[0] * 1e9) + hrtime[1]) / 1e6;
-    const ms: number = Math.round(sourceMS % 1000),
-        sec: number = Math.round(((sourceMS / 1000) % 60 + Number.EPSILON) * 100) / 100,
-        mins: number = Math.floor((sourceMS / (1000 * 60)) % 60),
-        hrs: number = Math.floor((sourceMS / (1000 * 60 * 60)) % 24);
+  let result = '';
+  const sourceMS: number = (hrtime[0] * 1e9 + hrtime[1]) / 1e6;
+  const ms: number = Math.round(sourceMS % 1000),
+    sec: number = Math.round((((sourceMS / 1000) % 60) + Number.EPSILON) * 100) / 100,
+    mins: number = Math.floor((sourceMS / (1000 * 60)) % 60),
+    hrs: number = Math.floor((sourceMS / (1000 * 60 * 60)) % 24);
 
-    if (hrs > 0){
-        result += hrs + ' hours ';
-    }
-    if (mins > 0){
-        result += mins + ' minutes ';
-    }
-    if (sec >= 1){
-        result += sec + ' seconds ';
-    }
-    if (mins === 0 && hrs === 0 && sec < 1 && ms > 0){
-        result += ms + 'ms';
+  if (hrs > 0) {
+    result += hrs + ' hours ';
+  }
+  if (mins > 0) {
+    result += mins + ' minutes ';
+  }
+  if (sec >= 1) {
+    result += sec + ' seconds ';
+  }
+  if (mins === 0 && hrs === 0 && sec < 1 && ms > 0) {
+    result += ms + 'ms';
+  }
+
+  return result;
+};
+class SimpleTimeLogger {
+  timers = new Map();
+
+  start(label: string) {
+    this.timers.set(label, process.hrtime());
+  }
+
+  end(label: string | null = null) {
+    if (label === null) {
+      label = Array.from(this.timers.keys()).pop();
     }
 
-    return result;
+    const timer = this.timers.get(label);
+    if (!timer) {
+      process.emitWarning(`No such label '${label}' for TimeLogger`);
+      return;
+    }
+
+    const diff = process.hrtime(timer);
+    const elapsedTime = convertHrTime(diff);
+    console.log(`${label ? label + ' ' : ''}${elapsedTime}`);
+    this.timers.delete(label);
+  }
 }
-class SimpleTimeLogger{
-    timers = new Map();
 
-    start(label:string){
-        this.timers.set(label, process.hrtime());
+export class TimeLogger {
+  timer: any;
+  label: string | null = null;
+  constructor(label: string | null = null) {
+    this.timer = process.hrtime();
+    if (label) {
+      this.label = label;
     }
+  }
 
-    end(label:string | null = null){
-        if (label === null){
-            label = Array.from(this.timers.keys()).pop();
-        }
-
-        const timer = this.timers.get(label);
-        if (!timer){
-            process.emitWarning(`No such label '${label}' for TimeLogger`);
-            return;
-        }
-
-        const diff = process.hrtime(timer);
-        const elapsedTime = convertHrTime(diff);
-        console.log(`${label ? label+' ': ''}${elapsedTime}`);
-        this.timers.delete(label);
+  end(label: string | null = null) {
+    const diff = process.hrtime(this.timer);
+    const elapsedTime = convertHrTime(diff);
+    if (!label) {
+      label = this.label;
     }
-}
-
-export class TimeLogger{
-    timer: any;
-    label: string | null = null;
-    constructor(label: string | null = null){
-        this.timer = process.hrtime();
-        if(label){
-            this.label = label;
-        }
-    }
-    
-    end(label: string | null = null){
-        const diff = process.hrtime(this.timer);
-        const elapsedTime = convertHrTime(diff);
-        if(!label){
-            label = this.label;
-        }
-        console.log(`${label ? label+' ': ''}${elapsedTime}`);
-    }
+    console.log(`${label ? label + ' ' : ''}${elapsedTime}`);
+  }
 }
 export const simpleTimeLogger = new SimpleTimeLogger();
