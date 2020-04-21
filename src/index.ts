198bec3989e127c770b173cb773dd11a5a1b3872
Vlad <vl.tansky@gmail.com>
2020-04-21 12:14:23 +0300

    typescript fixes

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,12 @@
-const convertHrTime = (hrtime: any) => {
-  let result = '';
-  const sourceMS: number = (hrtime[0] * 1e9 + hrtime[1]) / 1e6;
-  const ms: number = Math.round(sourceMS % 1000);
-  const sec: number = Math.round((((sourceMS / 1000) % 60) + Number.EPSILON) * 100) / 100;
-  const mins: number = Math.floor((sourceMS / (1000 * 60)) % 60);
-  const hrs: number = Math.floor((sourceMS / (1000 * 60 * 60)) % 24);
-
-  if (hrs > 0) {
-    result += hrs + ' hours ';
-  }
-  if (mins > 0) {
-    result += mins + ' minutes ';
-  }
-  if (sec >= 1) {
-    result += sec + ' seconds ';
-  }
-  if (mins === 0 && hrs === 0 && sec < 1 && ms > 0) {
-    result += ms + 'ms';
-  }
-
-  return result;
-};
-interface IElapsedLogger{
-  end(label:string):void;
-  get():string
+interface IElapsedLogger {
+  end(label: string): void;
+  get(): string;
 }
-class ElapsedLogger implements IElapsedLogger{
-  timer: any;
+type HrTime = [number, number];
+class ElapsedLogger implements IElapsedLogger {
+  private _timer: any;
   constructor() {
-    this.timer = process.hrtime();
+    this._timer = process.hrtime();
   }
 
   end(label: string = '') {
@@ -37,19 +15,43 @@ class ElapsedLogger implements IElapsedLogger{
   }
 
   get(): string {
-    const diff = process.hrtime(this.timer);
-    return convertHrTime(diff);
+    const diff: HrTime = process.hrtime(this._timer);
+    return this.convertHrTime(diff);
+  }
+
+  private convertHrTime(hrtime: HrTime): string {
+    let result = '';
+    const sourceMS: number = (hrtime[0] * 1e9 + hrtime[1]) / 1e6;
+    const ms: number = Math.round(sourceMS % 1000);
+    const sec: number = Math.round((((sourceMS / 1000) % 60) + Number.EPSILON) * 100) / 100;
+    const mins: number = Math.floor((sourceMS / (1000 * 60)) % 60);
+    const hrs: number = Math.floor((sourceMS / (1000 * 60 * 60)) % 24);
+
+    if (hrs > 0) {
+      result += hrs + ' hours ';
+    }
+    if (mins > 0) {
+      result += mins + ' minutes ';
+    }
+    if (sec >= 1) {
+      result += sec + ' seconds ';
+    }
+    if (mins === 0 && hrs === 0 && sec < 1 && ms > 0) {
+      result += ms + 'ms';
+    }
+
+    return result;
   }
 }
 class SimpleTimeLogger {
-  timers = new Map();
+  private _timers = new Map<string, IElapsedLogger>();
 
   start(label: string | null = null): IElapsedLogger {
     const elapsed = new ElapsedLogger();
     if (!label) {
       return elapsed;
     }
-    this.timers.set(label, elapsed);
+    this._timers.set(label, elapsed);
     return elapsed;
   }
 
@@ -57,11 +59,11 @@ class SimpleTimeLogger {
     const elapsedTime = this.get(label);
     const output = overrideLabel ? overrideLabel : label;
     console.log(`${output ? output + ' ' : ''}${elapsedTime}`);
-    this.timers.delete(label);
+    this._timers.delete(label);
   }
 
   get(label: any): string | boolean {
-    const timer = this.timers.get(label);
+    const timer = this._timers.get(label);
     if (!timer) {
       process.emitWarning(`No such label '${label}' for ElapsedLogger`);
       return false;
