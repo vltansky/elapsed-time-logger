68711407cd1f5bc46dc4b8a8f85f5d33236102d5
Vlad <vl.tansky@gmail.com>
2020-04-27 21:01:08 +0300

    import fixes + umd

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,12 @@
+import resolve from '@rollup/plugin-node-resolve';
+
 export default {
     input: 'lib/esm/index.js',
-    output: 'lib/umd/index.js',
     output: {
-        name: 'Elapssed-logger',
+        name: 'Elapsed_logger',
         file: 'lib/umd/index.js',
         format: 'umd',
         exports: "named",
-        globals: {
-          'browser-hrtime': 'hrtime',
-        }
     },
-    external: [ 'browser-hrtime' ]
+    plugins: [resolve()]
   };
